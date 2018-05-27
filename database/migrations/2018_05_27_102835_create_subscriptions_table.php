d70e504213b6fed5ace22cf5091d71949a248892
Eng <normalperson85@gmail.com>
2018-05-27 10:35:32 +0800

    added subscribtion table

---
+++ database/migrations/2018_05_27_102835_create_subscriptions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->nullable()->unsigned()->index();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status')->default('ACTIVE');
+            $table->text('remark');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
